3f8f08a9f220f0cffb874f428bb82dddb2f683a9
Raphael Morali-Bigras <raphaelmoralibigras@outlook.fr>
2023-06-21 09:20:32 -0400

    Creation of validation layers constant

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,8 @@ use raw_window_handle::HasRawDisplayHandle;
 use winit::event_loop::EventLoop;
 
 const EXTENSIONS: &[&str] = &["Test"];
+#[cfg(debug_assertions)]
+const VALIDATION_LAYERS: &[&str] = &["VK_LAYER_KHRONOS_validation"];
 
 #[allow(dead_code)]
 pub struct Application {
@@ -28,15 +30,32 @@ impl Application {
             .map(|ext| ext.as_ptr())
             .chain(winit_extension_names.iter().copied());
 
+        #[cfg(not(debug_assertions))]
         let instance = Self::create_instance(&entry, extension_names);
+
+        #[cfg(debug_assertions)]
+        let layers_cstring: Vec<CString> = VALIDATION_LAYERS
+            .iter()
+            .map(|&ext| CString::new(ext).unwrap())
+            .collect();
+        #[cfg(debug_assertions)]
+        let instance = {
+            let layer_names = layers_cstring.iter().map(|ext| ext.as_ptr());
+            Self::create_instance(&entry, extension_names, layer_names)
+        };
 
         Self { entry, instance }
     }
 
-    fn create_instance<T: IntoIterator<Item = *const i8>>(
+    fn create_instance<T, U>(
         entry: &Entry,
         extension_names: T,
-    ) -> Instance {
+        #[cfg(debug_assertions)] layer_names: U,
+    ) -> Instance
+    where
+        T: IntoIterator<Item = *const i8>,
+        U: IntoIterator<Item = *const i8>,
+    {
         // Define the vulkan application info
         let app_name = CString::new("Vulkan Tutorial").unwrap();
         let engine_name = CString::new("No Engine").unwrap();
