3001c6fd8b60b49eee851432a8f09bef656cb889
Raphael Morali-Bigras <raphaelmoralibigras@outlook.fr>
2023-06-22 16:03:57 -0400

    Completed validation layers support

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use std::ffi::{CStr, CString};
+use std::ffi::{c_void, CStr, CString};
 
-use ash::{vk, Entry, Instance};
+use ash::{extensions::ext, vk, Entry, Instance};
 use ash_window::enumerate_required_extensions;
 use raw_window_handle::HasRawDisplayHandle;
 use winit::event_loop::EventLoop;
@@ -12,11 +12,19 @@ const EXTENSIONS: &[&[u8]] = &[];
 
 #[cfg(debug_assertions)]
 const VALIDATION_LAYERS: &[&[u8]] = &[b"VK_LAYER_KHRONOS_validation\0"];
+#[cfg(debug_assertions)]
+const LAYER_SEVERITY: vk::DebugUtilsMessageSeverityFlagsEXT =
+    vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE;
 
 #[allow(dead_code)]
 pub struct Application {
     entry: Entry,
     instance: Instance,
+
+    #[cfg(debug_assertions)]
+    debug_util_ext: ext::DebugUtils,
+    #[cfg(debug_assertions)]
+    debug_messenger: vk::DebugUtilsMessengerEXT,
 }
 
 impl Application {
@@ -35,24 +43,33 @@ impl Application {
             );
 
         #[cfg(debug_assertions)]
-        let layer_name_temp = VALIDATION_LAYERS
+        let layer_names = VALIDATION_LAYERS
             .iter()
             .map(|&lay| CStr::from_bytes_with_nul(lay).unwrap().as_ptr())
             .collect::<Vec<*const i8>>();
 
         #[cfg(debug_assertions)]
-        let layer_names = Some(&*layer_name_temp);
+        let instance = { Self::create_instance(&entry, extension_names, &layer_names) };
         #[cfg(not(debug_assertions))]
-        let layer_names = None;
+        let instance = { Self::create_instance(&entry, extension_names) };
 
-        let instance = { Self::create_instance(&entry, extension_names, layer_names) };
-        Self { entry, instance }
+        #[cfg(debug_assertions)]
+        let (debug_util_ext, debug_messenger) = Self::setup_debug_messenger(&entry, &instance);
+
+        Self {
+            entry,
+            instance,
+            #[cfg(debug_assertions)]
+            debug_util_ext,
+            #[cfg(debug_assertions)]
+            debug_messenger,
+        }
     }
 
     fn create_instance<'a, T>(
         entry: &Entry,
         extension_names: T,
-        layer_names: Option<&[*const i8]>,
+        #[cfg(debug_assertions)] layer_names: &[*const i8],
     ) -> Instance
     where
         T: IntoIterator<Item = &'a CStr>,
@@ -110,23 +127,83 @@ impl Application {
             .application_info(&app_info)
             .enabled_extension_names(&extensions);
 
-        let create_info = match layer_names {
-            Some(names) => create_info_builder.enabled_layer_names(&names).build(),
-            None => create_info_builder.build(),
-        };
+        #[cfg(debug_assertions)]
+        let mut debug_messenger_create_info = Self::debug_messenger_create_info();
+        #[cfg(debug_assertions)]
+        let create_info = create_info_builder
+            .enabled_layer_names(&layer_names)
+            .push_next(&mut debug_messenger_create_info)
+            .build();
+
+        #[cfg(not(debug_assertions))]
+        let create_info = create_info_builder.build();
 
         // Create the instance
         unsafe { entry.create_instance(&create_info, None) }.unwrap()
     }
 
     #[cfg(debug_assertions)]
-    fn setup_debug_messenger() {}
+    fn setup_debug_messenger(
+        entry: &Entry,
+        instance: &Instance,
+    ) -> (ext::DebugUtils, vk::DebugUtilsMessengerEXT) {
+        let debug_util_ext = ext::DebugUtils::new(entry, instance);
+
+        let create_info = Self::debug_messenger_create_info();
+
+        let debug_util_messenger = unsafe {
+            debug_util_ext
+                .create_debug_utils_messenger(&create_info, None)
+                .unwrap()
+        };
+
+        (debug_util_ext, debug_util_messenger)
+    }
+
+    #[cfg(debug_assertions)]
+    fn debug_messenger_create_info() -> vk::DebugUtilsMessengerCreateInfoEXT {
+        vk::DebugUtilsMessengerCreateInfoEXT::builder()
+            .message_severity(
+                // vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
+                // | vk::DebugUtilsMessageSeverityFlagsEXT::INFO
+                vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+            )
+            .message_type(
+                vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                    | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+                    | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+            )
+            .pfn_user_callback(Some(Self::debug_callback))
+            .build()
+    }
+
+    #[cfg(debug_assertions)]
+    extern "system" fn debug_callback(
+        message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+        _message_types: vk::DebugUtilsMessageTypeFlagsEXT,
+        p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+        _p_user_data: *mut c_void,
+    ) -> vk::Bool32 {
+        if message_severity >= LAYER_SEVERITY {
+            let message = unsafe { CStr::from_ptr((*p_callback_data).p_message) };
+            eprintln!("Validation layer: {:?}", message);
+        }
+
+        vk::FALSE
+    }
 
     fn _main_loop() {
         todo!()
     }
 
     pub fn cleanup(&self) {
-        unsafe { self.instance.destroy_instance(None) };
+        unsafe {
+            #[cfg(debug_assertions)]
+            self.debug_util_ext
+                .destroy_debug_utils_messenger(self.debug_messenger, None);
+
+            self.instance.destroy_instance(None);
+        };
     }
 }
