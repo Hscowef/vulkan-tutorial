8b5c76b81498487da7ff162f419ef906eb94b686
Raphael Morali-Bigras <raphaelmoralibigras@outlook.fr>
2023-08-15 00:02:58 -0400

    Basic shaders and shader compilation

---
+++ build.rs
@@ -0,0 +1,22 @@
+use std::fs;
+use std::process::Command;
+
+const GLSLC_PATH: &str = "C:/VulkanSDK/1.3.250.0/Bin/glslc.exe";
+
+fn main() {
+    let paths = fs::read_dir("./src/shaders").unwrap();
+    for shader in paths {
+        let path = shader.unwrap().path();
+        let file_name = path.file_stem().unwrap();
+        let output_path: String = format!("./src/spirv/{}.spv", file_name.to_str().unwrap());
+
+        let output = Command::new(GLSLC_PATH)
+            .arg(path)
+            .arg("-o")
+            .arg(output_path)
+            .output()
+            .unwrap();
+
+        println!("{:?}", output);
+    }
+}
